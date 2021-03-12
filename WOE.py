359a87e5a71b0f01aa46230dab693988d1d36024
zaurrasulov <62173386+zaurrasulov@users.noreply.github.com>
2021-03-12 22:01:25 +0300

    Update WOE.py

--- WOE.py
+++ WOE.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
 
-df = pd.read_csv(r'C:\Users\Zaur\Desktop\healthcare-dataset-stroke-data.csv')
+df = pd.read_csv(r'...Desktop\healthcare-dataset-stroke-data.csv')
 
 # percentage of missing values
 print(df.isnull().mean())
